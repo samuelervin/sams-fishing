fe76bce0ab1bd9de46e6370eb977187ce623bafc
servin <samuel.ervin@wal-mart.com>
2020-05-18 10:27:17 -0500

    Updates

---
+++ precache-manifest.ae226e98ca716df288a7d28bbd45570f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60136aad8adb4f88e71169f91fceee06",
+    "url": "/sams-fishing/index.html"
+  },
+  {
+    "revision": "aa4fe01587b0d1a2bbcc",
+    "url": "/sams-fishing/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b6c1fc0a29214df21cad",
+    "url": "/sams-fishing/static/js/2.41eb4d49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sams-fishing/static/js/2.41eb4d49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa4fe01587b0d1a2bbcc",
+    "url": "/sams-fishing/static/js/main.64446685.chunk.js"
+  },
+  {
+    "revision": "3ed6e92226cd9d493455",
+    "url": "/sams-fishing/static/js/runtime-main.f45ca91a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sams-fishing/static/media/logo.5d5d9eef.svg"
+  }
+]);
