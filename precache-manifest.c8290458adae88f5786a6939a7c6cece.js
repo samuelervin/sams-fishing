e8fea10f49789ce7e5bc0f0d85367f1298d11f52
servin <samuel.ervin@wal-mart.com>
2020-05-27 16:57:12 -0500

    Updates

--- precache-manifest.c8290458adae88f5786a6939a7c6cece.js
+++ precache-manifest.c8290458adae88f5786a6939a7c6cece.js
@@ -1,23 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c1ef6c3bf0c6126456df2dbb69ed5c99",
+    "revision": "e55e8df6f2a3403e8af7f01ae27912cd",
     "url": "/sams-fishing/index.html"
   },
   {
-    "revision": "ef431ed377df9192ca44",
+    "revision": "faa9f80eb5f1a575d01c",
+    "url": "/sams-fishing/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5d23a82bf222c0a192ba",
     "url": "/sams-fishing/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "71039ebb95de74708881",
-    "url": "/sams-fishing/static/js/2.316da117.chunk.js"
+    "revision": "faa9f80eb5f1a575d01c",
+    "url": "/sams-fishing/static/js/2.5def1d3e.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/sams-fishing/static/js/2.316da117.chunk.js.LICENSE.txt"
+    "url": "/sams-fishing/static/js/2.5def1d3e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ef431ed377df9192ca44",
-    "url": "/sams-fishing/static/js/main.0dba721b.chunk.js"
+    "revision": "5d23a82bf222c0a192ba",
+    "url": "/sams-fishing/static/js/main.fdda5910.chunk.js"
   },
   {
     "revision": "3ed6e92226cd9d493455",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sams-fishing/precache-manifest.86348ee77c8a5b2ebafa85fca3c47674.js"
+  "/sams-fishing/precache-manifest.c8290458adae88f5786a6939a7c6cece.js"
 );
 
 self.addEventListener('message', (event) => {
